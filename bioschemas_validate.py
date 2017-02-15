6ca413b344416500ed15e4f0d643292f7d0b96eb
ficolo <fico89@gmail.com>
2017-02-15 13:12:10 +0000

    Adding type and cardinality validation

--- bioschemas_validate.py
+++ bioschemas_validate.py
@@ -1,25 +1,47 @@
 import urllib2
 from extruct.w3cmicrodata import MicrodataExtractor
+from extruct.jsonld import JsonLdExtractor
 from validator import validate_item
 import settings
 import click
+import json
+
+
+def normalize_jsonld(data):
+    if isinstance(data, dict):
+        item_type = data['@type']
+        item_context = data['@context']
+        data['type'] = item_context + '/' + item_type
+        del data['@type']
+        del data['@context']
+    elif isinstance(data, list):
+        for item in data:
+            normalize_jsonld(item)
 
 
 def validate_url(site_url):
-	response = urllib2.urlopen(site_url)
-	html = response.read()
-	mde = MicrodataExtractor()
-	data = mde.extract(html)
-	for item in data['items']:
-		if item['type'] in settings.supported_types:
-			click.secho('ITEM RETRIEVED: ' + str(item), fg='blue')
-			click.secho('VALIDATION OUTPUT: ' + str(validate_item(item)), fg='red', bold=True)
+    response = urllib2.urlopen(site_url)
+    html = response.read()
+    mde = MicrodataExtractor()
+    data = mde.extract(html)
+    if not data:
+        jslde = JsonLdExtractor()
+        data = jslde.extract(html)
+        normalize_jsonld(data)
+        print data
+    for item in data:
+        if item['type'] in settings.supported_types:
+            pretty_item = json.dumps(item, indent=4, sort_keys=True)
+            pretty_validation = json.dumps(validate_item(item), indent=4, sort_keys=True)
+            click.secho('ITEM RETRIEVED: ' + str(pretty_item), fg='blue')
+            click.secho('VALIDATION OUTPUT: ' + str(pretty_validation), fg='red', bold=True)
+
 
 @click.command()
 @click.argument('site_url')
 def run(site_url):
-	validate_url(site_url)
+    validate_url(site_url)
 
 
 if __name__ == "__main__":
-	run()
+    run()
--- validator.py
+++ validator.py
@@ -1,6 +1,7 @@
 from jsonschema.validators import extend
 from jsonschema.validators import Draft4Validator
 from jsonschema.exceptions import ValidationError
+from jsonschema._utils import format_as_index
 import json
 import re
 
@@ -31,18 +32,49 @@ Draft4ValidatorExtended = extend(
 )
 
 
+def is_integer(text):
+    try:
+        int(text)
+        return True
+    except ValueError:
+        return False
+
+
 def validate_item(item):
     file_name = item['type'].rsplit('/', 1)[1]
     schema = get_json(file_name + '.json')
     instance = item['properties']
     Draft4ValidatorExtended.check_schema(schema)
     validator = Draft4ValidatorExtended(schema)
-    validation = {'required_missing': [], 'recommended_missing': []}
+    validation = {'required_missing': [], 'recommended_missing': [],
+                  'bad_cardinality': [], 'bad_type': [], 'full_report': ''}
     for error in validator.iter_errors(instance):
         field = error.message.split("'")[1]
         if 'required' in error.message:
             validation['required_missing'].append(field)
-        if 'recommended' in error.message:
+        elif 'recommended' in error.message:
             validation['recommended_missing'].append(field)
+        elif error.validator == 'oneOf':
+            valid_types = []
+            for valid_schema in error.schema['oneOf']:
+                if valid_schema['type'] != 'object':
+                    valid_types.append(valid_schema['type'])
+                else:
+                    if 'enum' in valid_schema['properties']['type']:
+                        for valid_type in valid_schema['properties']['type']['enum']:
+                            valid_types.append(valid_type)
+            if isinstance(error.instance, list) and 'array' not in valid_types:
+                validation['full_report'] += 'Cardinality fail, you must provide only one value for: '\
+                                             + str(error.relative_path) + '\n'
+                validation['bad_cardinality'].append(error.relative_path[0])
+            elif is_integer(error.instance) and 'integer' in valid_types:
+                continue
+            else:
+                if 'array' in valid_types:
+                    valid_types.remove('array')
+                validation['bad_type'].append(error.relative_path[0])
+                validation['full_report'] += 'One of the ' + str(error.relative_path) +\
+                                             ' values is not any of the valid types: ' + str(valid_types) + '\n'
+                validation['full_report'] += ' Value: ' + str(error.instance)
     return validation
 
